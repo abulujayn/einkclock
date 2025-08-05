5d34e70c641ba1e51fa9dfea6245d8a8445e0655
abulujayn <zaeem@parkar.au>
2025-08-05 11:56:44 +1000

    add adhkar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import random
 from time import sleep
 from datetime import datetime
 from date import draw_date,draw_date2
@@ -6,78 +7,123 @@ from PIL import Image,ImageDraw,ImageFont
 
 from waveshare import epd2in13_V4
 
-running = True
-logging.basicConfig(level=logging.INFO)
 
-try:
-    logging.info("Starting EPD")
-    epd = epd2in13_V4.EPD()
-    logging.info("Initialising EPD")
-    epd.init()
-    epd.Clear(0xFF)
+# Create fonts
+font10 = ImageFont.truetype("font.ttf", 10)
+font12 = ImageFont.truetype("font.ttf", 12)
+font14 = ImageFont.truetype("font.ttf", 14)
+font16 = ImageFont.truetype("font.ttf", 16)
+font24 = ImageFont.truetype("font.ttf", 24)
+font48 = ImageFont.truetype("font.ttf", 48)
 
-    # No clue why these are reversed
-    width = epd.height
-    height = epd.width
 
-    # Create fonts
-    font10 = ImageFont.truetype("font.ttf", 10)
-    font12 = ImageFont.truetype("font.ttf", 12)
-    font14 = ImageFont.truetype("font.ttf", 14)
-    font16 = ImageFont.truetype("font.ttf", 16)
-    font24 = ImageFont.truetype("font.ttf", 24)
-    font48 = ImageFont.truetype("font.ttf", 48)
+def main():
+    running = True
+    logging.basicConfig(level=logging.INFO)
 
-    # Create image
-    base_image = Image.new("1", (width, height), 255)
-    draw = ImageDraw.Draw(base_image)
-    draw_date(draw, font16)
-    draw_date2(draw, 16, font10)
-    epd.displayPartBaseImage(epd.getbuffer(base_image.copy().rotate(180)))
+    try:
+        # Setup epd
+        logging.info("Starting EPD")
+        epd = epd2in13_V4.EPD()
+        logging.info("Initialising EPD")
+        epd.init()
+        epd.Clear(255)
 
-    while running:
-        # Make sure at least one minute has passed
-        if "time" in locals() and datetime.now().minute == time.minute:
-            continue
+        # No clue why these are reversed
+        width = epd.height
+        height = epd.width
 
-        # Go to sleep at midnight
-        if datetime.now().strftime("%H%M") == "0000":
-            image = Image.new("1", (width, height), 255)
+        # Create sleep image
+        sleep_image = create_sleep_image(width, height)
+
+        # Create image
+        base_image = create_base(width, height)
+        epd.displayPartBaseImage(epd.getbuffer(base_image.copy().rotate(180)))
+        reprint_base = False
+
+        while running:
+            # Make sure at least one minute has passed
+            if "time" in locals() and datetime.now().minute == time.minute:
+                continue
+
+            # Reprint base
+            if reprint_base:
+                base_image = create_base(width, height)
+                epd.displayPartBaseImage(epd.getbuffer(base_image.copy().rotate(180)))
+                reprint_base = False
+
+            time = datetime.now()
+
+            # Go to sleep at midnight
+            if time.strftime("%H%M") == "0000":
+                epd.displayPartBaseImage(epd.getbuffer(sleep_image))
+                logging.info("Going to sleep")
+                sleep(60 * 60 * 6) # 6 hours
+                logging.info("Waking up")
+                reprint_base = True
+                continue
+
+            # Show adhkar
+            if time.minute % 5 == 0 and random.random() < 0.5:
+                logging.info("Printing dhikr")
+                epd.displayPartBaseImage(epd.getbuffer(dhikr(width, height)))
+                reprint_base = True
+                continue
+
+            # Rereate image
+            image = base_image.copy()
             draw = ImageDraw.Draw(image)
-            draw.text((width / 2, height / 2), "Sleeping", fill = 0, font = font48, anchor = "mm")
-            epd.init()
-            epd.display(epd.getbuffer(image))
-            sleep(60 * 60 * 6) # 6 hours
-            # Create base image again
-            base_image = Image.new("1", (width, height), 255)
-            draw = ImageDraw.Draw(base_image)
-            draw_date(draw, font16)
-            draw_date2(draw, 16, font10)
-            epd.displayPartBaseImage(epd.getbuffer(base_image.copy().rotate(180)))
-            continue
+            # Draw time
+            time = datetime.now()
+            draw_time(time, draw, width, height)
+            # Rotate image
+            image = image.rotate(180)
 
-        # Rereate image
-        image = base_image.copy()
-        draw = ImageDraw.Draw(image)
-        # Draw time
-        time = datetime.now()
-        draw.text((width / 2, height / 2), time.strftime("%H:%M"), fill = 0, font = font48, anchor = "mm")
-        # Rotate image
-        image = image.rotate(180)
+            # Display image
+            if time.minute == 0:
+                # Full refresh every hour
+                epd.displayPartBaseImage(epd.getbuffer(base_image.copy().rotate(180)))
 
-        # Display image
-        if time.minute == 0:
-            # Full refresh every hour
-            epd.init()
-            epd.display(epd.getbuffer(image))
-        else:
             epd.displayPartial(epd.getbuffer(image))
 
-    epd.sleep()
-except IOError as e:
-    logging.error(e)
-except KeyboardInterrupt:
-    logging.info("Exiting")
-    epd2in13_V4.epdconfig.module_exit(cleanup=True)
-    running = False
+        epd.sleep()
+    except IOError as e:
+        logging.error(e)
+    except KeyboardInterrupt:
+        logging.info("Exiting")
+        epd2in13_V4.epdconfig.module_exit(cleanup=True)
+        running = False
 
+
+adhkar = ["الله أكبر", "الحمد لله", "سبحان الله", "لا إله إلا الله", "لا حول ولا قوة إلا بالله"]
+def dhikr(width, height):
+    image, draw = blank_image(width, height)
+    draw.text((width / 2, height / 2), random.choice(adhkar), fill = 0, font = font24, anchor = "mm")
+    return image.rotate(180)
+
+
+
+def create_base(width, height):
+    image, draw = blank_image(width, height)
+    draw_date(draw, font16)
+    draw_date2(draw, 16, font10)
+    return image
+
+
+def draw_time(time, draw, width, height):
+    draw.text((width / 2, height / 2), time.strftime("%H:%M"), fill = 0, font = font48, anchor = "mm")
+
+
+def create_sleep_image(width, height):
+    image, draw = blank_image(width, height)
+    draw.text((width / 2, height / 2), "Sleeping", fill = 0, font = font48, anchor = "mm")
+    return image.rotate(180)
+
+def blank_image(width, height):
+    image = Image.new("1", (width, height), 255)
+    draw = ImageDraw.Draw(image)
+    return image, draw
+
+
+if __name__ == "__main__":
+    main()
